4de21cbc5d0cbc8d4f0524629d2bcb9490dc0eb4
Ziad El-Fakhrany <ziad.fakhranyy@gmail.com>
2023-10-14 16:37:47 +0300

    files upload feature

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,16 +1,36 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete,  UseInterceptors, UploadedFile } from '@nestjs/common';
 import { TasksService } from './tasks.service';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { UpdateTaskDto } from './dto/update-task.dto';
 import { LazyModuleLoader } from '@nestjs/core'
 import { TasksModule } from './tasks.module';
 import { ApiAcceptedResponse, ApiTags, ApiResponse } from '@nestjs/swagger';
-
+import { FileInterceptor } from '@nestjs/platform-express';
+import { diskStorage } from 'multer';
+import { extname } from 'path';
 
 @Controller('tasks')
 @ApiTags('Tasks api')
 export class TasksController {
   constructor(private readonly lazyModuleLoader: LazyModuleLoader) {}
+
+  @Post('upload')
+  @UseInterceptors(FileInterceptor('file',{
+    storage: diskStorage({
+      destination: './files',
+      filename(req, file, callback){
+       const uniqueSuffix =
+          Date.now() + '-' + Math.round(Math.random() * 1e9);
+        const ext = extname(file.originalname);
+        const filename = `${uniqueSuffix}${ext}`;
+        callback(null, filename)
+      }
+    })
+  }))
+  handleUpload(@UploadedFile() file: Express.Multer.File){
+    console.log( file );
+  }
+
 
   @Post()
   async create(@Body() createTaskDto: CreateTaskDto) {
--- src/tasks/tasks.module.ts
+++ src/tasks/tasks.module.ts
@@ -1,11 +1,14 @@
 import { Module } from '@nestjs/common';
 import { TasksService } from './tasks.service';
 import { TasksController } from './tasks.controller';
+import { Task } from './entities/task.entity';  
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { Task } from './entities/task.entity';
+import { MulterModule } from '@nestjs/platform-express/multer/multer.module';
+
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Task])],  // to let me use the entity class as a repository in the service class
+  // we use typeOrmModule.forFeature to let me use the entity class as a repository in the service class
+  imports: [TypeOrmModule.forFeature([Task]), MulterModule.register({dest: './uploads'})],  
   controllers: [TasksController],
   providers: [TasksService],
 })
